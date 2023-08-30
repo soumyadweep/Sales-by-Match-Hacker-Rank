af636db47188d8b31ccec385a92cb90a024acbe7
soumyadweep <115583714+soumyadweep@users.noreply.github.com>
2023-08-31 02:27:04 +0530

    Create Sales_by_Match.py

---
+++ Sales_by_Match.py
@@ -0,0 +1,40 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'sockMerchant' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER_ARRAY ar
+#
+
+def sockMerchant(n, ar):
+    # Write your code here
+    ar.sort()
+    Dict = {}
+    pair = 0
+    for key in ar:
+        Dict[key] = Dict.get(key,0)+1
+    for key,value in Dict.items():
+        pair +=value//2
+    return pair
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = sockMerchant(n, ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
